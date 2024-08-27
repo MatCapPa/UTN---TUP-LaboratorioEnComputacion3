dbac6fb7685aa947dac9a6191777ec9bb7caad96
MatCapPa <cappa.nicolas1@gmail.com>
2024-08-27 11:46:30 -0300

    tup lab3 final

--- tup/src/main/java/ar/edu/utn/frbb/tup/persistence/CuentaDao.java
+++ tup/src/main/java/ar/edu/utn/frbb/tup/persistence/CuentaDao.java
@@ -8,9 +8,7 @@ import org.springframework.stereotype.Repository;
 
 import java.io.*;
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 @Repository
